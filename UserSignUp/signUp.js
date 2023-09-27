629ad1adcff6ef994d26282ece90b9fc0f9c94f1
emirylmz4144 <emiryilmaz4144@gmail.com>
2023-09-27 14:22:34 +0300

    UserSignUp Updated

--- UserSignUp/signUp.js
+++ UserSignUp/signUp.js
@@ -2,15 +2,34 @@ let formSucessDOM=document.querySelector("#userForm");
 formSucessDOM.addEventListener("submit",formSubmit);
 
 let userList=document.querySelector("#userList");
-// <li class="list-group-item list-group-item-action list-group-item-success">dfadf</li>
+
+let gonnaAlert=(important,message,className="danger")=>
+`<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+<strong>${important} </strong>${message}
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
+
+
+const ALERT=document.querySelector("#alert");
+
 function formSubmit(event)
 {
     event.preventDefault();
    
-    const E_MAİL=document.querySelector("#email").value;
-    const PASSWORD=document.querySelector("#password").value;
+    const E_MAİL=document.querySelector("#email");
+    const PASSWORD=document.querySelector("#password");
 
-    addToList(E_MAİL,PASSWORD)  
+    if(!(E_MAİL.value===''|| PASSWORD.value===''))
+    {
+        addToList(E_MAİL.value,PASSWORD.value)  
+        ALERT.innerHTML=gonnaAlert("Tebrikler! ","Form gönderme işlemi başarılı","success");
+    }
+    else
+    {
+        ALERT.innerHTML=gonnaAlert("HATALI GİRİŞ! ","Lütfen formu göndermek için boşlukları doldurun");
+    }
+    E_MAİL.value='';
+    PASSWORD.value='';
 }
 
 function addToList(email,password)
